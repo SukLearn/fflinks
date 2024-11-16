c339866a44b03bfdc1ebb5d8108bd7169463c942
SukLearnH90a <123197049+SukLearnH90a@users.noreply.github.com>
2024-11-16 18:30:57 +0400

    adding blob(fileUpload) function

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FFlinks from "./components/FFlinks/FFlinks";
 import ShowPage from "./components/ShowPage/ShowPage";
 import Lasha from "./components/1996/Lasha";
+import AvatarUploadPage from "./components/test/test";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<FFlinks />} />
           <Route path="show" element={<ShowPage />} />
           <Route path="lashaUrod" element={<Lasha />} />
-
+          <Route path="test" element={<AvatarUploadPage />} />
           <Route path="*" element={<FFlinks />} />
         </Routes>
       </Router>
